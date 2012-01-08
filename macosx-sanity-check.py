3214f969b7c84da4c954fcc6226f9b19e448b378
Marius Kintel <marius@kintel.net>
2012-01-08 13:49:58 +0100

    Minor bugfix to catch problems finding dependencies

--- macosx-sanity-check.py
+++ macosx-sanity-check.py
@@ -105,7 +105,7 @@ if __name__ == '__main__':
         dep = pending.pop()
         if DEBUG: print "Evaluating " + dep
         deps = find_dependencies(dep)
-        if DEBUG: print "  Deps: " + str(deps)
+        assert(deps)
         for d in deps:
             absfile = lookup_library(d)
             if absfile == None:
