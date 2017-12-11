0696181d1437a694d61f394e1dde3f969fa2fc19
Marius Kintel <marius@kintel.net>
2017-12-10 21:12:30 -0500

    cleanup

--- macosx-sanity-check.py
+++ macosx-sanity-check.py
@@ -24,23 +24,65 @@ import sys
 import os
 import subprocess
 import re
+from distutils.version import StrictVersion
+
+DEPLOYMENT_TARGET = StrictVersion('10.8')
+ARCHITECTURE = 'x86_64'
 
 DEBUG = False
 
 cxxlib = None
+executable_path = None
+lc_rpath = None
 
 def usage():
     print("Usage: " + sys.argv[0] + " <executable>", file=sys.stderr)
     sys.exit(1)
 
+def get_deployment_target(otool_output):
+    m = re.search("LC_VERSION_MIN_MACOSX.*\n(.*)\n\s+version (.*)", otool_output, re.MULTILINE)
+    return m.group(2)
+
+def get_rpath(otool_output):
+    m = re.search("LC_RPATH\n(.*)\n\s+path ([^ ]+)", otool_output, re.MULTILINE)
+    return m.group(2)
+
+def get_load_commands(binary):
+    p  = subprocess.Popen(["otool", "-l", binary], stdout=subprocess.PIPE, universal_newlines=True)
+    stdout, stderr = p.communicate()
+    if p.returncode != 0: return None, stderr
+    return stdout, None
+
+# Returns dependent libraries as a list of library filenames
+def get_libraries(binary):
+    p = subprocess.Popen(["otool", "-L", binary], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    output, err = p.communicate()
+    if p.returncode != 0: 
+        print("Failed with return code " + str(p.returncode) + ":")
+        print(err)
+        return None
+    return output.split('\n')
+
+def get_global_symbols(binary):
+    p  = subprocess.Popen(["nm", "-g", binary], stdout=subprocess.PIPE, universal_newlines=True)
+    stdout, stderr  = p.communicate()
+    if p.returncode != 0: return None, stderr
+    return stdout, None
+
+def verify_architecture(binary, arch):
+    p  = subprocess.Popen(["lipo", binary, "-verify_arch", arch], stdout=subprocess.PIPE, universal_newlines=True)
+    output = p.communicate()[0]
+    return p.returncode == 0
+
 # Try to find the given library by searching in the typical locations
 # Returns the full path to the library or None if the library is not found.
 def lookup_library(file):
     found = None
-    if re.search("@rpath", file):
+    if re.search("^@rpath", file):
         file = re.sub("^@rpath", lc_rpath, file)
-        if os.path.exists(file): found = file
-        if DEBUG: print("@rpath resolved: " + str(file))
+        if os.path.exists(file):
+            if DEBUG: print("@rpath resolved: " + str(file))
+            return file
     if not found:
         if re.search("\.app/", file):
             found = file
@@ -48,7 +90,7 @@ def lookup_library(file):
         elif re.search("@executable_path", file):
             abs = re.sub("^@executable_path", executable_path, file)
             if os.path.exists(abs): found = abs
-            if DEBUG: print("Lib in @executable_path found: " + str(found))
+            if DEBUG: print("Library in @executable_path found: " + str(found))
         elif re.search("\.framework/", file):
             found = os.path.join("/Library/Frameworks", file)
             if DEBUG: print("Framework found: " + str(found))
@@ -63,15 +105,7 @@ def lookup_library(file):
 def find_dependencies(file):
     libs = []
 
-    args = ["otool", "-L", file]
-    if DEBUG: print("Executing " + " ".join(args))
-    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-    output,err = p.communicate()
-    if p.returncode != 0: 
-        print("Failed with return code " + str(p.returncode) + ":")
-        print(err)
-        return None
-    deps = output.split('\n')
+    deps = get_libraries(file)
     for dep in deps:
         # print dep
         # Fail if libstc++ and libc++ was mixed
@@ -92,60 +126,49 @@ def find_dependencies(file):
     return libs
 
 def validate_lib(lib):
-    p  = subprocess.Popen(["otool", "-l", lib], stdout=subprocess.PIPE, universal_newlines=True)
-    output = p.communicate()[0]
-    if p.returncode != 0: return False
+    output, err = get_load_commands(lib)
+    if not output:
+        print('get_load_commands(): ' + err)
+        return False
+
     # Check deployment target
-    m = re.search("LC_VERSION_MIN_MACOSX.*\n(.*)\n\s+version (.*)", output, re.MULTILINE)
-    deploymenttarget = float(m.group(2))
-    if deploymenttarget > 10.8:
+    deploymenttarget = StrictVersion(get_deployment_target(output))
+    if deploymenttarget > DEPLOYMENT_TARGET:
         print("Error: Unsupported deployment target " + m.group(2) + " found: " + lib)
         return False
-# We don't support Snow Leopard anymore
-#    if re.search("LC_DYLD_INFO_ONLY", output):
-#        print "Error: Requires Snow Leopard: " + lib
-#        return False
-
+    
     # This is a check for a weak symbols from a build made on 10.12 or newer sneaking into a build for an
     # earlier deployment target. The 'mkostemp' symbol tends to be introduced by fontconfig.
-    p  = subprocess.Popen(["nm", "-g", lib], stdout=subprocess.PIPE, universal_newlines=True)
-    output = p.communicate()[0]
-    if p.returncode != 0: return False
+    output, err = get_global_symbols(lib)
+    if not output:
+        print('get_global_symbols(): ' + err)
+        return False
+
     match = re.search("mkostemp", output)
     if match:
         print("Error: Reference to mkostemp() found - only supported on macOS 10.12->")
-        return None
-
-    p  = subprocess.Popen(["lipo", lib, "-verify_arch", "x86_64"], stdout=subprocess.PIPE, universal_newlines=True)
-    output = p.communicate()[0]
-    if p.returncode != 0: 
-        print("Error: x86_64 architecture not supported: " + lib)
         return False
 
-# We don't support 32-bit binaries anymore
-#    p  = subprocess.Popen(["lipo", lib, "-verify_arch", "i386"], stdout=subprocess.PIPE, universal_newlines=True)
-#    output = p.communicate()[0]
-#    if p.returncode != 0: 
-#        print "Error: i386 architecture not supported: " + lib
-#        return False
+    if not verify_architecture(lib, ARCHITECTURE):
+        print('Error: ' + ARCHITECTURE + ' architecture not supported: ' + lib)
+        return False
+
     return True
 
-if __name__ == '__main__':
-    error = False
-    if len(sys.argv) != 2: usage()
-    executable = sys.argv[1]
+
+def process_executable(executable):
+    global executable_path, lc_rpath
+
     if DEBUG: print("Processing " + executable)
     executable_path = os.path.dirname(executable)
-
+    
     # Find the Runpath search path (LC_RPATH)
-    p  = subprocess.Popen(["otool", "-l", executable], stdout=subprocess.PIPE, universal_newlines=True)
-    output = p.communicate()[0]
-    if p.returncode != 0: 
-        print('Error otool -l failed on main executable')
-        sys.exit(1)
-    # Check deployment target
-    m = re.search("LC_RPATH\n(.*)\n\s+path ([^ ]+)", output, re.MULTILINE)
-    lc_rpath = m.group(2)
+    output, err = get_load_commands(executable)
+    if not output:
+        print('Error otool -l failed on main executable: ' + err)
+        return False
+
+    lc_rpath = get_rpath(output)
     if DEBUG: print('Runpath search path: ' + lc_rpath)
 
     # processed is a dict {libname : [parents]} - each parent is dependant on libname
@@ -156,18 +179,17 @@ if __name__ == '__main__':
         dep = pending.pop()
         if DEBUG: print("Evaluating " + dep)
         deps = find_dependencies(dep)
-#        if DEBUG: print "Deps: " + ' '.join(deps)
         assert(deps)
         for d in deps:
             absfile = lookup_library(d)
             if absfile == None:
                 print("Not found: " + d)
                 print("  ..required by " + str(processed[dep]))
-                error = True
+                return False
                 continue
             if not re.match(executable_path, absfile):
                 print("Error: External dependency " + d)
-                sys.exit(1)
+                return False
             if absfile in processed:
                 processed[absfile].append(dep)
             else: 
@@ -176,9 +198,19 @@ if __name__ == '__main__':
                 pending.append(absfile)
 
     for dep in processed:
-       if DEBUG: print("Validating: " + dep)
+       if DEBUG: print("--\nValidating: " + dep)
        if not validate_lib(dep):
-           print("..required by " + str(processed[dep]))
-           error = True
-    if error: sys.exit(1)
-    else: sys.exit(0)
+           print("Could not validate " + dep)
+           print("  ..required by " + str(processed[dep]))
+           return False
+
+    return True
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2: usage()
+
+    executable = sys.argv[1]
+
+    error = not process_executable(executable)    
+
+    sys.exit(error and 1 or 0)
